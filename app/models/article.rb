4fc982f27f8d74c5dd3a7dfc4a075e6f5db4a82f
Artur Lindholm <artur@cs.helsinki.fi>
2016-04-13 14:42:03 +0300

    article validations added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
 class Article < ActiveRecord::Base
 
+  validates :author, presence: true
+  validates :title, presence: true
+  validates :journal, presence: true
+  validates :year, numericality: { only_integer: true, less_than_or_equal_to: Date.today.year,
+                                   greater_than_or_equal_to: 0 }
+
   def key
     s = "" << self.author << ":" << self.year.to_s << ":" << "art" << self.id.to_s
   end
