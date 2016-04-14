6f01143c2275002226e48b35c136837e2ffc489b
Jussi Maristo <maot@cs.helsinki.fi>
2016-04-14 17:57:37 +0300

    fixed inproceeding migration

--- db/migrate/20160414144948_create_inproceedings.rb
+++ db/migrate/20160414144948_create_inproceedings.rb
@@ -1,9 +1,9 @@
 class CreateInproceedings < ActiveRecord::Migration
   def change
     create_table :inproceedings do |t|
-      t.string, :author
-      t.string, :title
-      t.string, :year
+      t.string :author
+      t.string :title
+      t.string :year
       t.string :booktitle
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414131140) do
+ActiveRecord::Schema.define(version: 20160414144948) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "author"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20160414131140) do
     t.string   "title"
     t.string   "publisher"
     t.integer  "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inproceedings", force: :cascade do |t|
+    t.string   "author"
+    t.string   "title"
+    t.string   "year"
+    t.string   "booktitle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
