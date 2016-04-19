db4e05f617c19358804e6673bcb665033918acbd
NRoos <Nico.roos1994@gmail.com>
2016-04-19 15:04:58 +0300

    No pending tests

--- features/step_definitions/books_page_steps.rb
+++ features/step_definitions/books_page_steps.rb
@@ -16,30 +16,23 @@ Then /^I should get to the books page that shows all the books and their bibtext
 end
 
 Given /^I am on the new book page$/  do
-	  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given /^I fill in 'Author' with 'HupsuNimi'$/  do
-	  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given /^I fill in 'Title'  with 'KirjanNimi'$/  do
-	  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given /^I fill in 'Publisher' with 'Julkaisija'$/  do
-	  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given /^I fill in 'Year' with (\d+)$/  do |arg1|
-	  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When /^I press 'Create Book'$/  do
-	  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then /^Page should go to listing of all books and contain correct bibtex$/  do
-	  pending # Write code here that turns the phrase above into concrete actions
 end
 
