0c110b9a05678cb842b6f1bc8362000bd35d6467
lauranor <laura.paajanen@helsinki.fi>
2016-04-20 13:11:52 +0300

    inproceedingtesting destroy-button

--- features/inproceedings.feature
+++ features/inproceedings.feature
@@ -9,3 +9,15 @@ Feature: Inproceedings Page
     And there exists an inproceeding in the database
     When I press the Inproceedings link
     Then I should get to page that shows all inproseedings and their bibtext
+
+  Scenario: Adds new inproceeding
+    Given I am on the new inproceeding page
+    And I fill inproceeding form
+    When I press 'create inproceeding' button
+    Then Should go to list of inproceedings and show the new inproceeding
+
+  Scenario: Destroys inproceeding correctly
+    Given I create new inproceeding
+    And I am on inproceedings page
+    When I press 'destroy' button
+    Then The list of inproceedings should not show created inproceeding
--- features/step_definitions/inproceedings_page_steps.rb
+++ features/step_definitions/inproceedings_page_steps.rb
@@ -14,3 +14,44 @@ end
 Then /^I should get to page that shows all inproseedings and their bibtext$/ do
   expect(page).to have_content("@inproceedings{Kivi:2004:inp1, author = {Kivi}, title = {Muuttolinnut pohjoisen pallonpuoliskon illassa}, booktitle = {Jeesus}, year = {2004}, }")
 end
+
+Given /^I am on the new inproceeding page$/ do
+  visit new_inproceeding_path
+end
+
+Given /^I fill inproceeding form$/ do
+  fill_in('Author', :with => "Koira")
+  fill_in('Title', :with => "Kevät")
+  fill_in('Year', :with => "1765")
+  fill_in('Booktitle', :with => "Vuodenajat")
+end
+
+When /^I press 'create inproceeding' button$/ do
+  click_button('Create Inproceeding')
+end
+
+Then /^Should go to list of inproceedings and show the new inproceeding$/ do
+  expect(page).to have_content("Inproceeding was successfully created.")
+  expect(page).to have_content("@inproceedings{Koira:1765:inp1, author = {Koira}, title = {Kevät}, booktitle = {Vuodenajat}, year = {1765}, }")
+end
+
+Given /^I create new inproceeding$/ do
+  visit new_inproceeding_path
+  fill_in('Author', :with => "Hiiri")
+  fill_in('Title', :with => "Juusto")
+  fill_in('Year', :with => "1981")
+  fill_in('Booktitle', :with => "Eläinten ravitsemustiede")
+  click_button('Create Inproceeding')
+end
+
+Given /^I am on inproceedings page$/ do
+  visit inproceedings_path
+end
+
+When /^I press 'destroy' button$/ do
+  click_on('Destroy')
+end
+
+Then /^The list of inproceedings should not show created inproceeding$/ do
+  page.should have_no_content("@inproceedings{Hiiri:1981:inp1, author = {Hiiri}, title = {Juusto}, booktitle = {Eläinten ravitsemustiede}, year = {1981}, }")
+end
