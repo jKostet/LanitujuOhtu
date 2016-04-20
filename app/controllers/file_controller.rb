aabe78a66e68a668d3ec71f7104609de7c3482ca
Merloni <tuomo.salmenkivi@gmail.com>
2016-04-20 12:26:55 +0300

    fixed a method that is not currently used2

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,10 +1,10 @@
 class FileController < ApplicationController
-  def write_bibtex_to_file
-    File.open("/public/raw.bib", 'w') do |f|
-      f.truncate(0)
-      
-    end
-  end
+  #def write_bibtex_to_file(content)
+  #  File.open("/public/raw.bib", 'w') do |f|
+  #    f.truncate(0)
+  #    f.write(content)
+  #  end
+  #end
   def downloadbibtex
     send_file("public/raw.bib", :type => 'text/plain', :disposition => 'attachment')
   end
