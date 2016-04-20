24f330db3e5f0c699c197ff12e4b1bba1753db33
NRoos <Nico.roos1994@gmail.com>
2016-04-20 19:19:22 +0300

    Created bibtex generator method for each type of reference, and wrote the bibtex generator

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,12 +1,23 @@
 class FileController < ApplicationController
-  #def write_bibtex_to_file(content)
-  #  File.open("/public/raw.bib", 'w') do |f|
-  #    f.truncate(0)
-  #    f.write(content)
-  #  end
-  #end
-  def downloadbibtex
-    send_file("public/raw.bib", :type => 'text/plain', :disposition => 'attachment')
+   def write_bibtex_to_file(content)
+     File.open("./public/raw.bib", 'w') do |f|
+       f.truncate(0)
+       f.write(content)
+     end
+   end
+  def downloadbibtex	
+	str = ""
+	Article.all.each do |a|
+		str = str << a.generateBibtex << "\n\n"
+	end
+	Book.all.each do |b|
+		str = str << b.generateBibtex << "\n\n"
+	end
+	Inproceeding.all.each do |i|
+		str = str << i.generateBibtex << "\n\n"
+	end
+	write_bibtex_to_file(str)
+	send_file("public/raw.bib", :type => 'text/plain', :disposition => 'attachment')
   end
 
 end
--- app/models/inproceeding.rb
+++ app/models/inproceeding.rb
@@ -3,4 +3,7 @@ class Inproceeding < ActiveRecord::Base
   def key
     s = "" << self.author << ":" << self.year.to_s << ":" << "inp" << self.id.to_s
   end
+  def generateBibtex
+	s= "@inproceeding" << self.key << ",\n" << "\tauthor = {" << self.author << "},\n" << "\ttitle = {" << self.title << "},\n" << "\tbooktitle = {" << self.booktitle << "},\n" << "\tyear = {" << self.year.to_s << "},\n}"
+  end
 end
