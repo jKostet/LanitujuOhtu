cf3a4f2bd21867b0642a21495e010bcd762d3af7
Artur Lindholm <artur@cs.helsinki.fi>
2016-04-21 10:14:50 +0300

    added validations for book and inproceedings

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ActiveRecord::Base
 
+	validates :author, presence: true
+	validates :title, presence: true
+	validates :publisher, presence: true
+	validates :year, numericality: { only_integer: true, less_than_or_equal_to: Date.today.year,
+                                   greater_than_or_equal_to: 0 }
+
   def key
     s = "" << self.author << ":" << self.year.to_s << ":" << "boo" << self.id.to_s
   end
